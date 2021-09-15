35c92b3fc1b45a95aa944b49b2894983a0559e14
정서연 <jjungseoyeon@naver.com>
2021-09-15 16:46:50 +0900

    Add key props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils"
 
 const foodLike = [
   {
+    id: 1,
     name: "ramen",
     image: "https://mblogthumb-phinf.pstatic.net/MjAxODA3MThfNjAg/MDAxNTMxOTAxNjU5Mjk2.TPvVTTjaTNDvIWUXB3Pwq6TeBvES4QBAmxgGmOSLaE4g.UshYkHzPL6LkBQrJzxwyQ17fhudRyV8gbbY75eootggg.PNG.daishin_blog/%ED%95%B4%EC%99%B8_%EC%9D%B8%EA%B8%B0_%ED%95%9C%EA%B5%AD_%EC%9D%8C%EC%8B%9D_01.png?type=w800"
   },
   {
+    id: 2,
     name: "pizza",
     image: "https://src.hidoc.co.kr/image/lib/2020/11/9/1604911318873_0.jpg"
   }
@@ -15,7 +17,7 @@ function App() {
   return (
     <div>
       {
-        foodLike.map(dish => (<Food name={dish.name} picture={dish.image} />))
+        foodLike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} />))
       }
     </div>
   )
