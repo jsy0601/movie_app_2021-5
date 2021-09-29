a017cd2e60609772d67ce7c043d979e1b1d4b8f9
정서연 <jjungseoyeon@naver.com>
2021-09-29 16:11:30 +0900

    setState() 함수로 count state 변경

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ class App extends Component {
     state = {
         count: 0
     }
+
+    add = () => {
+        this.setState({count: 1})
+    }
+
+    minus = () => {
+        this.setState({count: -1})
+    }
     render() {
         return (
+            <div>
             <h1>The number is: {this.state.count} </h1>
+            <button onClick={this.add}>Add</button>
+            <button onClick={this.minus}>Minus</button>
+            </div>
         )
     }
 }
