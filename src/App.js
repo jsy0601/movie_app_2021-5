ee7522c92b6878b913f8bbc61fe935a12d5936a8
정서연 <jjungseoyeon@naver.com>
2021-09-29 15:52:42 +0900

    state정의하고 count값 추가 후 사용하기

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Component} from 'react'
+
+class App extends Component {
+    state = {
+        count: 0
+    }
+    render() {
+        return (
+            <h1>The number is: {this.state.count} </h1>
+        )
+    }
+}
+
+export default App
