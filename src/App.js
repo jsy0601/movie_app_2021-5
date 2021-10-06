f4f18325bbeb67a31b60f08c3fd8cf505c97b4b3
정서연 <jjungseoyeon@naver.com>
2021-10-06 16:35:07 +0900

    getMovies() 함수

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ class App extends React.Component {
         isLoading: true,
         movies: []
     }
-    componentDidMount() {
-        axios.get('https://yts-proxy.now.sh/list_movies.json');
+
+    getMovies = async () => {
+        const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
     }
+
+    componentDidMount() {
+        this.getMovies()
+    }
+
     render() {
         const { isLoading } = this.state
         return (
