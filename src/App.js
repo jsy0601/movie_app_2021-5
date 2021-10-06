dd7af9ac5851040dbf3d6c811b4a331fdf93f71e
정서연 <jjungseoyeon@naver.com>
2021-10-06 16:11:10 +0900

    로딩 현상 구현하기

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from "react"
 
 class App extends React.Component {
     state = {
-        isLoading: true
+        isLoading: true,
+        movies: []
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ isLoading: false })
+        }, 6000)
     }
     render() {
         const { isLoading } = this.state
