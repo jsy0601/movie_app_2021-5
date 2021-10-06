2c339f2ea45e4e8412606583648b79e3c4c60667
정서연 <jjungseoyeon@naver.com>
2021-10-06 14:23:07 +0900

    영화 앱 만들기 워밍업

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
-import {Component} from 'react'
+import React from "react"
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        console.log('constructor')
-    }
-
-    componentDidMount() {
-        console.log('componentDidMount')
-    }
-
-    componentDidUpdate() {
-        console.log('componentDidUpdate')
-    }
-    
+class App extends React.Component {
     state = {
-        count: 0
+        isLoading: true
     }
-
-    add = () => {
-        this.setState(current => ({
-            count: current.count +1
-        }))
-    }
-
-    minus = () => {
-        this.setState(current => ({
-            count: current.count -1
-        }))
-    }
-
     render() {
-        console.log('render')
+        const { isLoading } = this.state
         return (
             <div>
-            <h1>The number is: {this.state.count} </h1>
-            <button onClick={this.add}>Add</button>
-            <button onClick={this.minus}>Minus</button>
+                { isLoading ? 'Loading...' : '영화 데이터 출력' }
             </div>
         )
     }
