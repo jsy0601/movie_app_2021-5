e44a31206a8785a6af3a98ef834c6e82e63786c7
정서연 <jjungseoyeon@naver.com>
2021-10-06 16:48:33 +0900

    객체에 있는 movies키에 접근하기

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ class App extends React.Component {
     }
 
     getMovies = async () => {
-        const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        const {
+            data: {
+                data: {movies},
+            },
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        console.log(movies)
     }
 
     componentDidMount() {
