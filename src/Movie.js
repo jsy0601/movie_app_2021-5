b8e0ff6879060789ee31fb80d353953d10956e0c
정서연 <jjungseoyeon@naver.com>
2021-10-27 14:24:37 +0900

    영화 장르 출력

--- src/Movie.js
+++ src/Movie.js
@@ -8,6 +8,11 @@ function Movie({title,year,summary,poster, genres}) {
             <div className="movie-data">
                 <h3 className="movie-title">{title}</h3>
                 <h5 className="movie-year">{year}</h5>
+                <ul className="movie-genres">
+                    {genres.map((genre, index) => {
+                        return <li key={index} className="movie-genre">{genre}</li>
+                    })}
+                </ul>
                 <p className="movie-summary">{summary}</p>
             </div>  
         </div>
